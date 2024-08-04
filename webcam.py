a93eae932de2fff47eb5a8c8d3f7bee1be72a3bf
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-04 20:14:40 +0300

    Added if __name__ == "__main__" to all files.

--- webcam.py
+++ webcam.py
@@ -1,17 +1,18 @@
 import cv2
 
-web_cam_video = cv2.VideoCapture(0)
-print("Streaming ...")
-try:
-    while True:
-        ret, frame = web_cam_video.read()
+if __name__ == "__main__":
+    web_cam_video = cv2.VideoCapture(0)
+    print("Streaming ...")
+    try:
+        while True:
+            ret, frame = web_cam_video.read()
 
-        cv2.imshow('Camera', frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
-except KeyboardInterrupt:
-    pass
-finally:
-    print("\nVideo disconnected")
-    web_cam_video.release()
-    cv2.destroyAllWindows()
+            cv2.imshow('Camera', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("\nVideo disconnected")
+        web_cam_video.release()
+        cv2.destroyAllWindows()
