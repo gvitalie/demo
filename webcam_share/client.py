8d47f785fd984e672f3fc8442c5b1253a72b0873
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-04 18:33:50 +0300

    Server share webcam video to multiple clients.

---
+++ webcam_share/client.py
@@ -0,0 +1,40 @@
+import socket
+import struct
+import pickle
+import cv2
+
+
+with socket.socket() as client:
+    client.connect(("localhost", 3456))
+    client_ip, client_port = client.getsockname()
+    print(f'Client {client_ip}:{client_port} connected')
+
+    try:
+        packets = b''
+        frame_size = 0
+        frame_index_size = struct.calcsize('Q')
+        while True:
+            packet = client.recv(4096)
+            packets += packet
+            if not packet:
+                print(client.getpeername(), 'disconnected')
+                break
+            if not frame_size:
+                frame_size = struct.unpack('Q', packets[:frame_index_size])[0]
+                packets = packets[frame_index_size:]
+            if len(packets) > frame_size:
+                frame_data = packets[:frame_size]
+                packets = packets[frame_size:]
+                frame_size = 0
+
+                frame = pickle.loads(frame_data)
+                cv2.imshow(f'{client_ip}:{client_port}', frame)
+                key = cv2.waitKey(1) == ord('q')
+                if key:
+                    break
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print(f'\nClient {client_ip}:{client_port} disconnected')
+        cv2.destroyAllWindows()
---
+++ webcam_share/server.py
@@ -0,0 +1,60 @@
+import socket
+import pickle
+import struct
+import threading
+
+import cv2
+
+
+def send_webcam_frame(clients):
+    web_cam_video = cv2.VideoCapture(0)
+    print("Streaming WebCam share ...")
+    try:
+        while True:
+            ret, frame = web_cam_video.read()
+            frame_data = pickle.dumps(frame)
+            frame_size = len(frame_data)
+            frame_data = struct.pack('Q', frame_size) + frame_data
+
+            if clients:
+                for client_id in clients:
+                    try:
+                        client_id.send(frame_data)
+                    except Exception as e:
+                        print(e, f'-> Client or network may be down')
+                        clients.remove(client_id)
+                        if not clients:
+                            break
+            else:
+                break
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Video disconnected\n")
+        web_cam_video.release()
+
+
+with socket.socket() as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('localhost', 3456))
+    server.listen()
+    print('Server listen\n')
+
+    try:
+        clients = list()
+        while True:
+            client, address = server.accept()
+            print(address, 'connected')
+
+            if not clients:
+                send_webcam_frame_thread = threading.Thread(target=send_webcam_frame,
+                                                        args=[clients])
+                send_webcam_frame_thread.daemon = True
+                send_webcam_frame_thread.start()
+            clients.append(client)
+
+    except KeyboardInterrupt:
+        print('\nServer down')
+    finally:
+        pass
