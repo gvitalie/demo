7a66d682a75d96a8d043191ba03d8126eb8d3aa8
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-08 17:26:20 +0300

    Added microphone example.

--- stream_microphone.py
+++ stream_microphone.py
@@ -1,6 +1,3 @@
-#############
-# Example 1 #
-#############
 # Usage — python-sounddevice, version 0.4.7
 # https://python-sounddevice.readthedocs.io/en/0.4.7/usage.html#callback-streams
 
@@ -18,17 +15,3 @@ print('Listening microphone')
 with sd.Stream(channels=2, callback=callback):
     sd.sleep(int(duration * 1000))
 
-
-# Example 2
-# import sounddevice as sd
-# duration = 5.5  # seconds
-#
-#
-# def callback(indata, outdata, frames, time, status):
-#     if status:
-#         print(status)
-#     outdata[:] = indata
-#
-#
-# with sd.RawStream(channels=2, dtype='int24', callback=callback):
-#     sd.sleep(int(duration * 1000))
