cce2943422fed579bd97e93f2a1249881d609063
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-09 20:54:21 +0300

    Added description to README.

--- stream_microphone.py
+++ stream_microphone.py
@@ -11,7 +11,17 @@ def callback(indata, outdata, frames, time, status):
     outdata[:] = indata
 
 
+samplerate = 44100
+blocksize = 10
+latency = 'high'
+channels = 2
+
 print('Listening microphone')
-with sd.Stream(channels=2, callback=callback):
-    sd.sleep(int(duration * 1000))
+with sd.Stream(channels=channels,
+               callback=callback,
+               samplerate=samplerate,
+               blocksize=blocksize,
+               never_drop_input=False):
+    # sd.sleep(int(duration * 1000))
+    input("Press any key to stop ...")
 
