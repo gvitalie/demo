5bd37139ee3ff0575b737c6a11f346be573db4a3
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-20 09:29:37 +0300

    DSP - digital signal processing.

--- DSP/compose_signals.py
+++ DSP/compose_signals.py
@@ -11,17 +11,17 @@ npSin1 = np.sin(2 * np.pi * signFreq1 * npTime)
 npSin2 = np.sin(2 * np.pi * signFreq2 * npTime)
 cleanSignal = npSin1 + npSin2
 
-plt.subplot(811)
+plt.subplot(421)
 plt.plot(npTime, npSin1, linewidth=1, label=str(signFreq1) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(812)
+plt.subplot(422)
 plt.plot(npTime, npSin2, linewidth=1, label=str(signFreq2) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(813)
+plt.subplot(423)
 plt.plot(npTime, cleanSignal, linewidth=1,
          label="Signal " + str(signFreq1) + "Hz and " + str(signFreq2) + "Hz combined")
 plt.margins(x=0, y=1)
@@ -31,18 +31,18 @@ plt.legend()
 noise = 2.5 * np.random.randn(len(npTime))
 noiseSignal = cleanSignal + noise
 
-plt.subplot(814)
+plt.subplot(424)
 plt.plot(noise, linewidth=1, label="Noise")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(815)
+plt.subplot(425)
 plt.plot(npTime,noiseSignal, linewidth=1, label="Signal with noise")
 plt.margins(x=0, y=1)
 plt.legend()
 # plt.show()
 
-plt.subplot(816)
+plt.subplot(426)
 n = len(npTime)
 yf = rfft(noiseSignal)
 xf = rfftfreq(n,step)
@@ -50,7 +50,7 @@ plt.plot(np.abs(yf), linewidth=1,label="DFT")
 plt.margins(x=0, y=0.5)
 plt.legend()
 
-plt.subplot(817)
+plt.subplot(427)
 yf_abs = np.abs(yf)
 indices = yf_abs>300   # filter out those value under 300
 yfClean = indices * yf # noise frequency will be set to 0
@@ -58,7 +58,7 @@ plt.plot(np.abs(yfClean), linewidth=1, label="DFT Removed Noise Frequencies")
 plt.margins(x=0, y=0.5)
 plt.legend()
 
-plt.subplot(818)
+plt.subplot(428)
 newFClean = irfft(yfClean)
 plt.plot(npTime, newFClean, linewidth=1, label="DFT inversed back to Time signal")
 plt.margins(x=0, y=1)
