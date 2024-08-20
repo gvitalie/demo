ff5712426b4cb2d107600a09caf30220c44b9135
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-20 09:22:02 +0300

    DSP - digital signal processing.

--- DSP/compose_signals.py
+++ DSP/compose_signals.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fft import rfft, rfftfreq
+from scipy.fft import rfft, rfftfreq, irfft
 
 signFreq1 = 27
 signFreq2 = 35
@@ -11,17 +11,17 @@ npSin1 = np.sin(2 * np.pi * signFreq1 * npTime)
 npSin2 = np.sin(2 * np.pi * signFreq2 * npTime)
 cleanSignal = npSin1 + npSin2
 
-plt.subplot(611)
+plt.subplot(811)
 plt.plot(npTime, npSin1, linewidth=1, label=str(signFreq1) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(612)
+plt.subplot(812)
 plt.plot(npTime, npSin2, linewidth=1, label=str(signFreq2) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(613)
+plt.subplot(813)
 plt.plot(npTime, cleanSignal, linewidth=1,
          label="Signal " + str(signFreq1) + "Hz and " + str(signFreq2) + "Hz combined")
 plt.margins(x=0, y=1)
@@ -31,22 +31,36 @@ plt.legend()
 noise = 2.5 * np.random.randn(len(npTime))
 noiseSignal = cleanSignal + noise
 
-plt.subplot(614)
+plt.subplot(814)
 plt.plot(noise, linewidth=1, label="Noise")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(615)
+plt.subplot(815)
 plt.plot(npTime,noiseSignal, linewidth=1, label="Signal with noise")
 plt.margins(x=0, y=1)
 plt.legend()
 # plt.show()
 
-plt.subplot(616)
+plt.subplot(816)
 n = len(npTime)
 yf = rfft(noiseSignal)
 xf = rfftfreq(n,step)
-plt.plot(xf, np.abs(yf), linewidth=1,label="DFT")
+plt.plot(np.abs(yf), linewidth=1,label="DFT")
 plt.margins(x=0, y=0.5)
+plt.legend()
+
+plt.subplot(817)
+yf_abs = np.abs(yf)
+indices = yf_abs>300   # filter out those value under 300
+yfClean = indices * yf # noise frequency will be set to 0
+plt.plot(np.abs(yfClean), linewidth=1, label="DFT Removed Noise Frequencies")
+plt.margins(x=0, y=0.5)
+plt.legend()
+
+plt.subplot(818)
+newFClean = irfft(yfClean)
+plt.plot(npTime, newFClean, linewidth=1, label="DFT inversed back to Time signal")
+plt.margins(x=0, y=1)
 plt.legend()
 plt.show()
