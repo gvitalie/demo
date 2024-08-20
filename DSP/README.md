adbab13f62d16aad52fa78a7144c7dc517563cfe
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-20 09:04:34 +0300

    DSP - digital signal processing.

--- DSP/README.md
+++ DSP/README.md
@@ -5,3 +5,6 @@
 - [x] [Fast Fourier Transform (FFT)](https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.03-Fast-Fourier-Transform.html)
 - [x] [FFT in Python](https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter24.04-FFT-in-Python.html)
 
+# Tutorial
+- [x] [Fourier transform in Python](https://medium.com/@martinkarlsson.io/fourier-transform-in-python-f15b3243ce9a)
+- [x] [Fourier transform in Python - Source Code](https://github.com/martinkarlssonio/fourier-transform-python/blob/main/workspace/fourier.ipynb)
--- DSP/compose_signals.py
+++ DSP/compose_signals.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.fft import rfft, rfftfreq
 
 signFreq1 = 27
 signFreq2 = 35
@@ -10,17 +11,17 @@ npSin1 = np.sin(2 * np.pi * signFreq1 * npTime)
 npSin2 = np.sin(2 * np.pi * signFreq2 * npTime)
 cleanSignal = npSin1 + npSin2
 
-plt.subplot(411)
+plt.subplot(611)
 plt.plot(npTime, npSin1, linewidth=1, label=str(signFreq1) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(412)
+plt.subplot(612)
 plt.plot(npTime, npSin2, linewidth=1, label=str(signFreq2) + "Hz")
 plt.margins(x=0, y=1)
 plt.legend()
 
-plt.subplot(413)
+plt.subplot(613)
 plt.plot(npTime, cleanSignal, linewidth=1,
          label="Signal " + str(signFreq1) + "Hz and " + str(signFreq2) + "Hz combined")
 plt.margins(x=0, y=1)
@@ -30,9 +31,22 @@ plt.legend()
 noise = 2.5 * np.random.randn(len(npTime))
 noiseSignal = cleanSignal + noise
 
-plt.subplot(414)
+plt.subplot(614)
+plt.plot(noise, linewidth=1, label="Noise")
+plt.margins(x=0, y=1)
+plt.legend()
+
+plt.subplot(615)
 plt.plot(npTime,noiseSignal, linewidth=1, label="Signal with noise")
 plt.margins(x=0, y=1)
 plt.legend()
-plt.show()
+# plt.show()
 
+plt.subplot(616)
+n = len(npTime)
+yf = rfft(noiseSignal)
+xf = rfftfreq(n,step)
+plt.plot(xf, np.abs(yf), linewidth=1,label="DFT")
+plt.margins(x=0, y=0.5)
+plt.legend()
+plt.show()
