b1c60a55ccaf74dc49acde4203719b89c578ce42
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-23 13:56:25 +0300

    Updated to use queue.deque.

--- idea/algorithm.py
+++ idea/algorithm.py
@@ -8,11 +8,13 @@ seed(kernel)
 
 tsec = 1
 for i in range(3):
+
     a = [randint(10, 99) for i in range(10)]
     print(a)
+
     q = queue.deque()
-    q.append(a.pop())
-    for i in range(1, len(a)):
+
+    for i in range(1, len(a)+1):
         temp = a.pop()
         index = bisect.bisect_right(q, temp)
         q.insert(index, temp)
