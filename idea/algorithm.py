a8e47ed1583289d65872daa1b8e8a175fa7c9bab
Vitalie Ghelbert <gvitalie@gmail.com>
2024-08-23 17:05:47 +0300

    Updated to use queue.deque.

--- idea/algorithm.py
+++ idea/algorithm.py
@@ -2,21 +2,42 @@ import bisect
 from random import randint, seed
 from time import sleep
 import queue
+import time
+import matplotlib.pyplot as plt
 
 kernel = 13
 seed(kernel)
 
 tsec = 1
-for i in range(3):
 
-    a = [randint(10, 99) for i in range(10)]
-    print(a)
+y = list()
+x = list()
+n = 5
+for k in range(1, 10 ** n, 10000):
+
+    a = [randint(10, 99) for i in range(k)]
+    x.append(k)
+    print(k)
+    # print(a)
 
     q = queue.deque()
 
+    start = time.time()
     for i in range(len(a)):
         temp = a.pop()
         index = bisect.bisect_right(q, temp)
         q.insert(index, temp)
-    print(q, '\n')
+
+    end = time.time()
+    print(end-start)
+    y.append(end-start)
+    # print(q, '\n')
     sleep(tsec)
+
+
+plt.plot(x, y)
+# plt.xscale('log')
+plt.xlabel('Vector size')
+plt.ylabel('Time (sec)')
+plt.grid()
+plt.show()
